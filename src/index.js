7f1692547c794bfc18f418ed35e59aca99dd7e8d
shashiweb <shashiweb64@gmail.com>
2024-03-30 16:49:29 +0530

    create new project in weekend

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Myhome from   './pages/Home';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Myhome></Myhome>
+
+  </React.StrictMode>
+);
+
+
